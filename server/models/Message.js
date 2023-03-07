55beb6f7a9bd26e848438f09775f5741d8eebd16
DoanTran2001 <dadotran3@gmail.com>
2023-03-07 20:58:34 +0700

    Server

---
+++ server/models/Message.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose"
+const Schema = mongoose.Schema
+
+const MessageSchema = new Schema({
+  sender: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  receiver: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true,
+  }
+}, {timestamps: true})
+
+export default mongoose.model('Message', MessageSchema)
